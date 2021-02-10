b158b2c13c4b3fcfaa5df7acf65a84b2aeb9097d
Snikimonkd <cepminkis132@gmail.com>
2021-02-11 00:16:04 +0300

    recursion+jsdoc

--- source/plainify.js
+++ source/plainify.js
@@ -1,25 +1,43 @@
 'use strict';
 
+/**
+ * Функция получает на вход обычный объект с вложенными свойствами, а вохвращает plain-объект
+ * @param {Object} obj - объект с вложенными свойствами
+ * @returns {Object} newObj - plain-объект, полученный из obj
+ * @example 
+ * Получает на вход:
+ * const nected = {
+ *      deep: {
+ *          foo: 'bar',
+ *          baz: 42
+ *      }
+ * };
+ * Возвращает:
+ * const plain = {
+ *      'deep.foo': 'bar',
+ *      'deep.baz': 42
+ * };
+ */
+
 const plainify = obj => {
-    var ret = {};
-    var flag = true;
-    while (flag) {
-        var newObj = Object();
-        flag = false;
-        for (var key in obj) {
-            if (typeof obj[key] === 'object') {
-                for (var nextKey in obj[key]) {
-                    if (typeof obj[key][nextKey] == 'object') {
-                        flag = true;
-                    }
-                    newObj[key + '.' + nextKey] = obj[key][nextKey];
+    let newObj = new Object();
+    let isNested = false;
+    for (let key in obj) {
+        if (typeof obj[key] === 'object' && obj[key] != null) {
+            for (let nextKey in obj[key]) {
+                if (typeof obj[key][nextKey] === 'object' && obj[key][nextKey] != null) {
+                    isNested = true;
                 }
-            } else {
-                newObj[key] = obj[key];
+                newObj[key + '.' + nextKey] = obj[key][nextKey];
             }
+        } else {
+            newObj[key] = obj[key];
         }
-        obj = newObj;
     }
 
-    return obj;
+    if (isNested) {
+        newObj = plainify(newObj);
+    }
+
+    return newObj;
 }
